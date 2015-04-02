05f415311de3229c4e0120b3737ac2436f27e364
Don McLamb <mclamb.donald@gmail.com>
2015-04-02 09:54:21 -0400

    saving what I have

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,8 +9,8 @@ require_relative '../lib/ship'
 
 #bundle exec ruby
 
-cells_x = [0,1,2,3,4,5,6,7,8,9]
-cells_y = [0,1,2,3,4,5,6,7,8,9]
+# cells_x = [0,1,2,3,4,5,6,7,8,9]
+# cells_y = [0,1,2,3,4,5,6,7,8,9]
 #
 # # @cells_x = cells_x
 # # @cells_y = cells_y
@@ -26,115 +26,146 @@ ocean = Ocean.create([
   ])
 
 
-
-
+# def build_ocean
+  cells_x = [0,1,2,3,4,5,6,7,8,9]
+  cells_y = [0,1,2,3,4,5,6,7,8,9]
 
   cells_x.each do |x|
     cells_y.each do |y|
+      #new line for ships info
+      # ship_one_location_array.each do |x_i,y|
+
       atlantic = Ocean.find_by(name: "Atlantic")
-      atlantics_ships = atlantic.cells.create([
+      atlantics_ships = atlantic.cells.create
         (x:x, y:y)
 
 
 
-        ])
+
+        # x = x_i.Cell.find_by(x: x_i)
+        #   y =
+
       end
     end
-
-    cells_array = []
-    random_selection = []
-
-    cells_x.each do |x|
-      cells_y.each do |y|
-        cells_array << x
-        cells_array << y
-
-        random_selection = cells_array.sample(4)
-      end
-    end
-
-    ship_loc1 = random_selection[0..1]
-    ship_loc2 = random_selection[2..4]
+# ship_loc1_end
 
 
-    random_selection do |x,y,w,z|
+# def ships_location
+#   cells_x = [0,1,2,3,4,5,6,7,8,9]
+#   cells_y = [0,1,2,3,4,5,6,7,8,9]
+#     cells_array = []
+#     random_selection = []
+#
+#     cells_x.each do |x|
+#       cells_y.each do |y|
+#         cells_array << x
+#         cells_array << y
+#
+#
+#       end
+#     end
+#
+# #random_selection = ships_location.sample(4)
+#     random_selection = cells_array.sample(4)
+#
+#     # ship_loc1 = random_selection[0..1]
+#     # ship_loc2 = random_selection[2..4]
+#
+#
+#
+#
+#     # assign random ship location to ship_loc1 and ship_loc2
+#     # random_selection (array) is the random ship location
+#     # take the first 2 elements to ship1
+#     # take the last 2 elements to ship2
+#
+#
+#
+#
+#
+#     ship_direction = ["up","down", "left", "right"]
+#
+#     selected_direction1 = ship_direction.sample(1)
+#
+#     ship1_loc_begin = random_selection[0..1]
+#
+#     if selected_direction1 == "up"
+#       ship1_loc1_end = ship1_loc_begin[0], ship1_loc_begin[1] += 1
+#       ship1_loc2_end = ship1_loc_begin[0], ship1_loc_begin[1] += 2
+#       ship1_loc3_end = ship1_loc_begin[0], ship1_loc_begin[1] += 3
+#       ship1_loc4_end = ship1_loc_begin[0], ship1_loc_begin[1] += 4
+#       ship1_loc5_end = ship1_loc_begin[0], ship1_loc_begin[1] += 5
+#       # ship_loc1_end = ship_loc1_begin[0], ship_loc1_begin[1] += 1
+#       # ship_loc1_end = ship_loc1_begin[0], ship_loc1_begin[1] += 2
+#       # ship_loc1_end = ship_loc1_begin[0], ship_loc1_begin[1] += 3
+#       # ship_loc1_end = ship_loc1_begin[0], ship_loc1_begin[1] += 4
+#       # ship_loc1_end = ship_loc1_begin[0], ship_loc1_begin[1] += 5
+#     elsif selected_direction1 == "down"
+#       ship1_loc1_end = ship1_loc_begin[0], ship1_loc_begin[1] -= 1
+#       ship1_loc2_end = ship1_loc_begin[0], ship1_loc_begin[1] -= 2
+#       ship1_loc3_end = ship1_loc_begin[0], ship1_loc_begin[1] -= 3
+#       ship1_loc4_end = ship1_loc_begin[0], ship1_loc_begin[1] -= 4
+#       ship1_loc5_end = ship1_loc_begin[0], ship1_loc_begin[1] -= 5
+#     elsif selected_direction1 == "left"
+#       ship1_loc1_end = ship1_loc_begin[0] -= 1, ship1_loc_begin[1]
+#       ship1_loc2_end = ship1_loc_begin[0] -= 2, ship1_loc_begin[1]
+#       ship1_loc3_end = ship1_loc_begin[0] -= 3, ship1_loc_begin[1]
+#       ship1_loc4_end = ship1_loc_begin[0] -= 4, ship1_loc_begin[1]
+#       ship1_loc5_end = ship1_loc_begin[0] -= 5, ship1_loc_begin[1]
+#     else #selected_direction == "right"
+#       ship1_loc1_end = ship1_loc_begin[0] += 1, ship1_loc_begin[1]
+#       ship1_loc2_end = ship1_loc_begin[0] -= 2, ship1_loc_begin[1]
+#       ship1_loc3_end = ship1_loc_begin[0] -= 3, ship1_loc_begin[1]
+#       ship1_loc3_end = ship1_loc_begin[0] -= 4, ship1_loc_begin[1]
+#       ship1_loc3_end = ship1_loc_begin[0] -= 5, ship1_loc_begin[1]
+#     end
+#
+#     selected_direction2 = ship_direction.sample(1)
+#
+#     ship_loc2_begin = random_selection[2..4]
+#     if selected_direction2 == "up"
+#       ship_loc2_end = ship_loc2_begin[0], ship_loc2_begin[1] += 5
+#     elsif selected_direction2 == "down"
+#       ship_loc2_end = ship_loc2_begin[0], ship_loc2_begin[1] -= 5
+#     elsif selected_direction2 == "left"
+#       ship_loc2_end = ship_loc2_begin[0] -= 5, ship_loc2_begin[1]
+#     else #selected_direction == "right"
+#       ship_loc2_end = ship_loc2_begin[0] += 5, ship_loc2_begin[1]
+#     end
+#
+#     # if selected_direction2 == "up"
+#     #   ship_loc2_begin[1] +=5
+#     #   #add consecutively to array
+#     # elsif selected_direction2 == "down"
+#     #   ship_loc2_begin[1] -=5
+#     # elsif selected_direction == "left"
+#     #   ship_loc2_begin[0] -=5
+#     # else #selected_direction == "right"
+#     #   ship_loc2_begin[0] +=5
+#     # end
 
-      ship_loc1 = "x,y"
-      ship_loc2 = "w,z"
-    end
+def ships
 
-    ship_loc1 = random_selection.take(2)
-    ship_loc2 = random_selection.pop(2)
+atlantic = Ocean.find_by(name: "Atlantic")
+atlantics_ships = atlantic.ships.create([
 
 
-    # assign random ship location to ship_loc1 and ship_loc2
-    # random_selection (array) is the random ship location
-    # take the first 2 elements to ship1
-    # take the last 2 elements to ship2
-
-    random_selection
-
-    random_selection[0..1]
+  ])
 
 
-    ocean.ship?s = []
-
-
-    ship_direction = ["up","down", "left", "right"]
-
-    selected_direction1 = ship_direction.sample(1)
-
-    ship_loc1_begin = random_selection[0..1]
-
-    if selected_direction1 == "up"
-      ship_loc1_end = ship_loc1_begin[0], ship_loc1_begin[1] += 5
-    elsif selected_direction1 == "down"
-      ship_loc1_end = ship_loc1_begin[0], ship_loc1_begin[1] -= 5
-    elsif selected_direction1 == "left"
-      ship_loc1_end = ship_loc1_begin[0] -= 5, ship_loc1_begin[1]
-    else #selected_direction == "right"
-      ship_loc1_end = ship_loc1_begin[0] += 5, ship_loc1_begin[1]
-    end
-
-    selected_direction2 = ship_direction.sample(1)
-
-    ship_loc2_begin = random_selection[2..4]
-    if selected_direction1 == "up"
-      ship_loc2_end = ship_loc2_begin[0], ship_loc2_begin[1] += 5
-    elsif selected_direction1 == "down"
-      ship_loc2_end = ship_loc2_begin[0], ship_loc2_begin[1] -= 5
-    elsif selected_direction1 == "left"
-      ship_loc2_end = ship_loc2_begin[0] -= 5, ship_loc2_begin[1]
-    else #selected_direction == "right"
-      ship_loc2_end = ship_loc2_begin[0] += 5, ship_loc2_begin[1]
-    end
-
-    # if selected_direction2 == "up"
-    #   ship_loc2_begin[1] +=5
-    #   #add consecutively to array
-    # elsif selected_direction2 == "down"
-    #   ship_loc2_begin[1] -=5
-    # elsif selected_direction == "left"
-    #   ship_loc2_begin[0] -=5
-    # else #selected_direction == "right"
-    #   ship_loc2_begin[0] +=5
-    # end
+end
 
 
 
-
-
-
-
-
-    puts random_selection
-    ships = [[1,1,1,1,1]]
-    ships2 = [[1,1,1,1,1]]
-
-    ships.select { |num|}
-
-
-    ships
+    #
+    # puts random_selection
+    # ships = [[1,1,1,1,1]]
+    # ships2 = [[1,1,1,1,1]]
+    #
+    # ships.select { |num|}
+    #
+    #
+    # ships
 
     atlantic = Ocean.find_by(name: "Atlantic")
     atlantics_ships = atlantic.cells.create([
--- lib/ship.rb
+++ lib/ship.rb
@@ -8,12 +8,100 @@ class Ship < ActiveRecord::Base
 
 
 
-  # attr_accessor :length :name :sunk?
-  # def initiaialize(length, name, sunk?)
-  #   @length = length
-  #   @name = name
-  #   @sunk = sunk
-  # end
+  def ships_location
+    cells_x = [0,1,2,3,4,5,6,7,8,9]#short term fix for border, change to 4 & 5
+    cells_y = [0,1,2,3,4,5,6,7,8,9]#short term fix for border, change to 4 & 5
+      cells_array = []
+      random_selection = []
+
+      cells_x.each do |x|
+        cells_y.each do |y|
+          cells_array << x
+          cells_array << y
+
+
+        end
+      end
+
+  #random_selection = ships_location.sample(4)
+      random_selection = cells_array.sample(4)
+
+      # ship_loc1 = random_selection[0..1]
+      # ship_loc2 = random_selection[2..4]
+
+
+
+
+      # assign random ship location to ship_loc1 and ship_loc2
+      # random_selection (array) is the random ship location
+      # take the first 2 elements to ship1
+      # take the last 2 elements to ship2
+
+
+
+
+
+      ship_direction = ["up","down", "left", "right"]
+
+      selected_direction1 = ship_direction.sample(1)
+
+      ship1_loc_begin = random_selection[0..1]
+
+      if selected_direction1 == "up"
+        ship1_loc1_end = ship1_loc_begin[0], ship1_loc_begin[1] += 1
+        ship1_loc2_end = ship1_loc_begin[0], ship1_loc_begin[1] += 2
+        ship1_loc3_end = ship1_loc_begin[0], ship1_loc_begin[1] += 3
+        ship1_loc4_end = ship1_loc_begin[0], ship1_loc_begin[1] += 4
+        ship1_loc5_end = ship1_loc_begin[0], ship1_loc_begin[1] += 5
+        # ship_loc1_end = ship_loc1_begin[0], ship_loc1_begin[1] += 1
+        # ship_loc1_end = ship_loc1_begin[0], ship_loc1_begin[1] += 2
+        # ship_loc1_end = ship_loc1_begin[0], ship_loc1_begin[1] += 3
+        # ship_loc1_end = ship_loc1_begin[0], ship_loc1_begin[1] += 4
+        # ship_loc1_end = ship_loc1_begin[0], ship_loc1_begin[1] += 5
+      elsif selected_direction1 == "down"
+        ship1_loc1_end = ship1_loc_begin[0], ship1_loc_begin[1] -= 1
+        ship1_loc2_end = ship1_loc_begin[0], ship1_loc_begin[1] -= 2
+        ship1_loc3_end = ship1_loc_begin[0], ship1_loc_begin[1] -= 3
+        ship1_loc4_end = ship1_loc_begin[0], ship1_loc_begin[1] -= 4
+        ship1_loc5_end = ship1_loc_begin[0], ship1_loc_begin[1] -= 5
+      elsif selected_direction1 == "left"
+        ship1_loc1_end = ship1_loc_begin[0] -= 1, ship1_loc_begin[1]
+        ship1_loc2_end = ship1_loc_begin[0] -= 2, ship1_loc_begin[1]
+        ship1_loc3_end = ship1_loc_begin[0] -= 3, ship1_loc_begin[1]
+        ship1_loc4_end = ship1_loc_begin[0] -= 4, ship1_loc_begin[1]
+        ship1_loc5_end = ship1_loc_begin[0] -= 5, ship1_loc_begin[1]
+      else #selected_direction == "right"
+        ship1_loc1_end = ship1_loc_begin[0] += 1, ship1_loc_begin[1]
+        ship1_loc2_end = ship1_loc_begin[0] -= 2, ship1_loc_begin[1]
+        ship1_loc3_end = ship1_loc_begin[0] -= 3, ship1_loc_begin[1]
+        ship1_loc3_end = ship1_loc_begin[0] -= 4, ship1_loc_begin[1]
+        ship1_loc3_end = ship1_loc_begin[0] -= 5, ship1_loc_begin[1]
+      end
+
+      selected_direction2 = ship_direction.sample(1)
+
+      ship_loc2_begin = random_selection[2..4]
+      if selected_direction2 == "up"
+        ship_loc2_end = ship_loc2_begin[0], ship_loc2_begin[1] += 5
+      elsif selected_direction2 == "down"
+        ship_loc2_end = ship_loc2_begin[0], ship_loc2_begin[1] -= 5
+      elsif selected_direction2 == "left"
+        ship_loc2_end = ship_loc2_begin[0] -= 5, ship_loc2_begin[1]
+      else #selected_direction == "right"
+        ship_loc2_end = ship_loc2_begin[0] += 5, ship_loc2_begin[1]
+      end
+
+      # if selected_direction2 == "up"
+      #   ship_loc2_begin[1] +=5
+      #   #add consecutively to array
+      # elsif selected_direction2 == "down"
+      #   ship_loc2_begin[1] -=5
+      # elsif selected_direction == "left"
+      #   ship_loc2_begin[0] -=5
+      # else #selected_direction == "right"
+      #   ship_loc2_begin[0] +=5
+      # end
+
 
 end
 
