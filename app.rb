e93098990674e1b375f4832d74fad884e381b995
Don McLamb <mclamb.donald@gmail.com>
2015-04-02 12:04:04 -0400

    got cells working again

--- app.rb
+++ app.rb
@@ -1,10 +1,28 @@
 require 'pry'
 require 'active_record'
 require_relative 'db/connection'
-# require_relative '../lib/ocean'
 # require_relative '../lib/'
 # require_relative '../lib/'
+require_relative './lib/cell'
+require_relative './lib/ship'
+require_relative './lib/ocean'
+require_relative './lib/linebreak'
+require_relative './lib/string_gets'
 
+
+
+def menu
+
+linebreak
+String_That_Gets.gets_string("""HELLO, WELCOME TO THE GAME OF GAMES!
+WOULD YOU LIKE TO PLAY BATTLESHIP!!!
+Please Respond Yes Or No
+""")
+linebreak
+
+return
+
+end
 
 
 # def menu
@@ -20,4 +38,3 @@ require_relative 'db/connection'
 #
 #
 #
-binding.pry
